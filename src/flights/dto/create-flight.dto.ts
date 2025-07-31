2c2cd94240fc4e60717bdf732778ed282ce7dde6
nomi035 <noamantariq@gmail.com>
2025-07-31 12:23:25 +0500

    flights updated

--- src/flights/dto/create-flight.dto.ts
+++ src/flights/dto/create-flight.dto.ts
@@ -2,28 +2,86 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateFlightDto {
 @ApiProperty()
-    tripType:string
+tripType:string
 
 @ApiProperty()
-    from:string
+from:string
 
 @ApiProperty()
-    to:string
+to:string
 
 @ApiProperty()
-    departureDate: Date;
-
-  @ApiProperty()
-    returnDate?: Date;
+departureDate: Date;
 
 @ApiProperty()
-    arrivalDate:Date
+returnDate: Date;
 
 @ApiProperty()
-    flightClass:string
+arrivalDate:Date
 
 @ApiProperty()
-    fare:number
+flightClass:string
+
+
 @ApiProperty()
-    companyName:string
+flightNumber:string
+
+@ApiProperty()
+departureAirport:string
+
+@ApiProperty()
+arrivalAirport:string
+
+@ApiProperty()
+departureTime:Date
+
+@ApiProperty()
+arrivalTime:Date
+
+@ApiProperty()
+airline:string
+
+@ApiProperty()
+basePrice:number
+
+@ApiProperty()
+taxPrice:number
+
+@ApiProperty()
+totalPrice:number
+
+@ApiProperty()
+currency:string
+
+@ApiProperty()
+cancellationAllowedUntill:string
+
+@ApiProperty()
+isRefundable:boolean
+
+@ApiProperty()
+cancellationPenalty:number
+
+@ApiProperty()
+voidableUntil:Date;
+
+
+@ApiProperty()
+passengerType:string;
+
+@ApiProperty()
+passportRequired:boolean;
+
+@ApiProperty()
+seatSelectionAllowed:boolean;
+
+@ApiProperty()
+recheckBagsRequired:boolean;
+
+@ApiProperty()
+checkedBaggage:string
+
+@ApiProperty()
+cabbinBaggage:string;
+
 }
